f05ab434308726bd4500b42deae8b5635d1592f2
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-10 17:53:35 -0300

    feat: :sparkles: Added related_films field to planets collection

--- src/v1/models/planet-model.js
+++ src/v1/models/planet-model.js
@@ -5,6 +5,7 @@ const PlanetSchema = new mongoose.Schema(
     name: { type: String, required: true },
     climate: { type: String, required: true },
     terrain: { type: String, required: true },
+    related_films: { type: Number, required: true },
     created_at: { type: Date, required: true, default: Date.now },
     updated_at: { type: Date, required: true, default: Date.now }
   },
