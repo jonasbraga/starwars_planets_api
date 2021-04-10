02dda3b639dc05aad3d7fbd72ec4c332f7f3672c
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-10 16:18:19 -0300

    feat: :sparkles: New isObjectId validation

--- src/v1/validations/index.js
+++ src/v1/validations/index.js
@@ -1,3 +1,7 @@
 const isEmpty = object => !Object.entries(object || {}).length
+const isObjectId = id => !!id.match(/^[0-9a-fA-F]{24}$/)
 
-module.exports = { isEmpty }
+module.exports = {
+  isEmpty,
+  isObjectId
+}
