916db7761957ecd01a381ba6caa1fa83eee0800a
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-10 16:26:33 -0300

    feat: :card_file_box: Create Planet model for manipulate database registers

---
+++ src/v1/models/planet-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const PlanetSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    climate: { type: String, required: true },
+    terrain: { type: String, required: true },
+    created_at: { type: Date, required: true, default: Date.now },
+    updated_at: { type: Date, required: true, default: Date.now }
+  },
+  { collection: 'planets' }
+)
+
+const Planet = mongoose.model('Planet', PlanetSchema)
+module.exports = { Planet }
