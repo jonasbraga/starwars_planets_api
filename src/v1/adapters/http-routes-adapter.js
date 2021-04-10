243a8dc7601de9ad5ffce8cafed28e930abf551c
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-10 16:20:28 -0300

    chore: :art: Changed default value for empty params in the request
    Changed from null to empty object

--- src/v1/adapters/http-routes-adapter.js
+++ src/v1/adapters/http-routes-adapter.js
@@ -3,9 +3,9 @@ const { isEmpty } = require('../validations')
 const adaptRoute = (controller) => {
   return async (req, res) => {
     const httpRequest = {
-      body: isEmpty(req.body) ? null : req.body,
-      pathParams: isEmpty(req.params) ? null : req.params,
-      queryParams: isEmpty(req.query) ? null : req.query
+      body: isEmpty(req.body) ? {} : req.body,
+      pathParams: isEmpty(req.params) ? {} : req.params,
+      queryParams: isEmpty(req.query) ? {} : req.query
     }
 
     const httpResponse = await controller.handle(httpRequest)
