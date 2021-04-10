14d288d8f6a188319a22a17245f7f65ac8f6e5e0
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-09 21:22:44 -0300

    feat: :heavy_plus_sign: Added dotenv dependency

--- src/v1/index.js
+++ src/v1/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const planets = require('./routes/planets-routes')
 
