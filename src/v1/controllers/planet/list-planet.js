55e9a502508c18ea5490932e7b510043b409c3ba
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-10 16:30:30 -0300

    feat: :sparkles: List planets feature added

--- src/v1/controllers/planet/list-planet.js
+++ src/v1/controllers/planet/list-planet.js
@@ -1,7 +1,24 @@
+const { Planet } = require('../../models/planet-model')
 const httpResponse = require('../../helpers/http-response-helper')
+const { isObjectId } = require('../../validations')
 
 const handle = async httpRequest => {
-  return httpResponse.ok({ message: 'Hello World List!' })
+  try {
+    const { pathParams: { id } } = httpRequest
+
+    if (id) {
+      if (!isObjectId(id)) return httpResponse.badRequest({ message: 'Invalid ID' })
+      const planets = await Planet.findById(id)
+      return planets
+        ? httpResponse.ok(planets)
+        : httpResponse.notFound({ message: `No planet found with id: ${id}` })
+    }
+
+    const planets = await Planet.find()
+    return httpResponse.ok({ planets })
+  } catch (error) {
+    return httpResponse.serverError({ message: error.message })
+  }
 }
 
 module.exports = { handle }
