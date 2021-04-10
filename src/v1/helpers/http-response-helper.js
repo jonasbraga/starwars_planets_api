0ab460000578d5549608bc6568d35436a62ebe3c
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-09 23:23:54 -0300

    feat: :sparkles: New http response helper

---
+++ src/v1/helpers/http-response-helper.js
@@ -0,0 +1,24 @@
+import { StatusCodes } from 'http-status-codes'
+
+const _makeHttpResponse = (statusCode, body = {}) => ({ statusCode, body })
+
+const ok = body => _makeHttpResponse(StatusCodes.OK, body)
+
+const created = body => _makeHttpResponse(StatusCodes.CREATED, body)
+
+const noContent = () => _makeHttpResponse(StatusCodes.NO_CONTENT)
+
+const badRequest = body => _makeHttpResponse(StatusCodes.BAD_REQUEST, body)
+
+const notFound = body => _makeHttpResponse(StatusCodes.NOT_FOUND, body)
+
+const serverError = body => _makeHttpResponse(StatusCodes.INTERNAL_SERVER_ERROR, body)
+
+module.exports = {
+  ok,
+  created,
+  noContent,
+  badRequest,
+  notFound,
+  serverError
+}
