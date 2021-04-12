9379afe5f50ca3a809469b2a46083e8706cb9be6
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-12 01:10:53 -0300

    feat: :sparkles: Returning the database connection in db config

--- src/v1/config/db.config.js
+++ src/v1/config/db.config.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(process.env.DB_CONNECTION_STRING, {
+const dbConnection = mongoose.connect(process.env.DB_CONNECTION_STRING, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 }).then(() => {
   console.log('Database connected with the force successfully!')
+  return mongoose.connection
 }, err => {
   console.log(`Database connect error: ${err}`)
   process.exit()
 })
+
+module.exports = { dbConnection }
