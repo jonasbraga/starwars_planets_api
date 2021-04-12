71c23d9a81054b2ccf531ef3d8d232a8621d0baa
Jonas Braga <jonasbraga2001@gmail.com>
2021-04-12 01:09:29 -0300

    refactor: :recycle: Removed some unuseful code in the app file

--- src/v1/app.js
+++ src/v1/app.js
@@ -2,14 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
-const mongooseConnection = require('./config/db.config')
+require('./config/db.config')
 
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
-app.set('mongoose connection', mongooseConnection)
 
 const { planetsRoutes } = require('./routes/planets-routes')
 
